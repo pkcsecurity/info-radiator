6704563a9a873e0df0c36db07ae8f1d3f93f433c
Jonathan Ming <jming@pkcsecurity.com>
2019-05-23 09:58:47 -0700

    Yeah!

--- launch-radiator.sh
+++ launch-radiator.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-THINGS=(https://give.imb.org file:///home/pkc/radiatorimages/inarush.png https://pkc.io)
+THINGS=(https://give.imb.org https://pkc.io)
+
+shopt -s nullglob
+shopt -s nocaseglob
+for file in /home/pkc/info-radiator/images/*.{png,jpg,jpeg,gif}
+do
+    THINGS+=("file://$file")
+done
+shopt -u nullglob
+shopt -u nocaseglob
 
 sleep 3
 
 firefox --no-remote "${THINGS[@]/#/}" &
-xdotool search --sync --onlyvisible --class "Firefox" windowactivate windowfocus key F11
+xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
 xdotool mousemove --screen 0 0 0
 
 while true
