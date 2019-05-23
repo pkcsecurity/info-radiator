62303095c8ec61b33034e27fd209a5a38bb0cfca
Jonathan Ming <jming@pkcsecurity.com>
2019-05-23 11:50:33 -0700

    Sleep-wait for wi-fi moved to radiate command

--- launch-radiator.sh
+++ launch-radiator.sh
@@ -11,8 +11,6 @@ done
 shopt -u nullglob
 shopt -u nocaseglob
 
-sleep 3
-
 firefox --no-remote "${THINGS[@]/#/}" &
 xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
 xdotool mousemove --screen 0 0 0
