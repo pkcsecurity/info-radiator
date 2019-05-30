68e54d914043684f788e152615f9aac56382e0cd
Jonathan Ming <jming@pkcsecurity.com>
2019-05-30 16:45:15 -0700

    Add VR Tic Tac Toe, and shuffle the list of items before opening

--- launch-radiator.sh
+++ launch-radiator.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-THINGS=(https://pkc.io https://give.imb.org https://pkc.io/blog)
+# Start with URLs to open
+things=(https://pkc.io https://give.imb.org https://pkc.io/blog https://vr-tic-tac-toe.herokuapp.com)
 
-shopt -s nullglob
+# Grab files from the images directory as well
+shopt n-s nullglob
 shopt -s nocaseglob
 for file in /home/pkc/info-radiator/images/*.{png,jpg,jpeg,gif}
 do
-    THINGS+=("file://$file")
+    things+=("file://$file")
 done
 shopt -u nullglob
 shopt -u nocaseglob
 
+# Shuffle the things array
+# (help from https://www.linuxquestions.org/questions/linux-general-1/randomly-shuffling-of-array-elements-837184/#post4124234)
+num=$(( ${#things[@]} -1 ))
+newseq=$( shuf -i 0-$num )
+shuffled=()
+for newi in $newseq; do
+    shuffled+=("${things[$newi]}")
+done
+
+# Start the X window system automation
 xdotool mousemove --screen 0 1500 400
-firefox --no-remote "${THINGS[@]/#/}" &
+firefox --no-remote "${shuffled[@]/#/}" &
 xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
 xdotool mousemove --screen 0 0 0
 
@@ -20,4 +32,3 @@ while true
 do
     xdotool key ctrl+Next sleep 10
 done
-
